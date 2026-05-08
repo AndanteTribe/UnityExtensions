6f8806b01093f8b43d2fd57578553e0f484b5a95
Rope <roperaganmen88@gmail.com>
2026-05-08 20:58:44 +0900

    fix: レビュー修正

--- src/UnityExtensions.Unity/Assets/Test/RectTransformTest.cs
+++ src/UnityExtensions.Unity/Assets/Test/RectTransformTest.cs
@@ -10,22 +10,22 @@ namespace UnityExtensions.Tests
         private RectTransform _rectTransform = null!;
 
         [OneTimeSetUp]
-        public void BeforeAll()
+        public void OneTimeSetUp()
         {
             var targetGameObject = new GameObject("Test");
             _rectTransform = targetGameObject.AddComponent<RectTransform>();
         }
 
         [Test]
-        [TestCase(100f,100f)]
-        [TestCase(0f,0f)]
+        [TestCase(100f, 100f)]
+        [TestCase(0f, 0f)]
         [TestCase(-100f, -100f)]
         [TestCase(float.MaxValue, float.MinValue)]
         [TestCase(float.Epsilon, -float.Epsilon)]
-        [TestCase(0.1f,0.01f)]
-        [TestCase(-0.1f,1.5f)]
-        [TestCase(-50f,0f)]
-        [TestCase(0f,75f)]
+        [TestCase(0.1f, 0.01f)]
+        [TestCase(-0.1f, 1.5f)]
+        [TestCase(-50f, 0f)]
+        [TestCase(0f, 75f)]
         public void SetSizeFloatTest(float width, float height)
         {
             _rectTransform.SetSize(width, height);
